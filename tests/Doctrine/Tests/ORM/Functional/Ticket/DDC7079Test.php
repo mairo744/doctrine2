3df05cf8e07a051fd79d7b63bd14a517524a4644
mairo744 <mairo744@gmail.com>
2019-11-04 22:39:45 +0100

    Incorrect table name for join tables in sqlite db when it contains schema attribute.
    Join table name doesnt depending on the platform.
    Table name was "schema.table" instead of "schema__table".
    
    (cherry picked from commit 4878cd3f4ef30ffc6047c18e0f7b16aafeabc3b4)

---
+++ tests/Doctrine/Tests/ORM/Functional/Ticket/DDC7079Test.php
@@ -0,0 +1,126 @@
+<?php
+declare(strict_types=1);
+
+namespace Doctrine\Tests\ORM\Functional\Ticket;
+
+use Doctrine\Common\Persistence\Mapping\RuntimeReflectionService;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Mapping\DefaultQuoteStrategy;
+use Doctrine\Tests\OrmFunctionalTestCase;
+
+/**
+ * Class DDC7079Test
+ * @group DDC-7079
+ * @package Doctrine\Tests\ORM\Functional
+ * @author mairo744
+ */
+class DDC7079Test extends OrmFunctionalTestCase
+{
+    /**
+     * @var \Doctrine\ORM\Mapping\DefaultQuoteStrategy
+     */
+    private $strategy;
+
+    /**
+     * @var \Doctrine\DBAL\Platforms\AbstractPlatform
+     */
+    private $platform;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->platform = $this->_em->getConnection()->getDatabasePlatform();
+        $this->strategy = new DefaultQuoteStrategy();
+    }
+
+    public function testGetTableName ()
+    {
+        $table = [
+            'name'=>'cms_user',
+            'schema' => 'cms',
+        ];
+
+        $cm = $this->createClassMetadata(DDC7079CmsUser::class);
+        $cm->setPrimaryTable($table);
+
+        $this->assertEquals($this->getTableFullName($table), $this->strategy->getTableName($cm, $this->platform));
+    }
+
+    public function testJoinTableName()
+    {
+        $table = [
+            'name'=>'cmsaddress_cmsuser',
+            'schema' => 'cms',
+        ];
+
+        $cm = $this->createClassMetadata(DDC7079CmsAddress::class);
+        $cm->mapManyToMany(
+            [
+                'fieldName'     => 'user',
+                'targetEntity'  => 'DDC7079CmsUser',
+                'inversedBy'    => 'users',
+                'joinTable'     => $table,
+            ]
+        );
+
+        $this->assertEquals($this->getTableFullName($table), $this->strategy->getJoinTableName($cm->associationMappings['user'], $cm, $this->platform));
+    }
+
+    private function getTableFullName(array $table): string
+    {
+        $join = '.';
+        if ( ! $this->platform->supportsSchemas() && $this->platform->canEmulateSchemas()) {
+            $join = '__';
+        }
+
+        return $table['schema'] . $join . $table['name'];
+    }
+
+    private function createClassMetadata(string $className): ClassMetadata
+    {
+        $cm = new ClassMetadata($className);
+        $cm->initializeReflection(new RuntimeReflectionService());
+
+        return $cm;
+    }
+}
+
+/**
+ * @Entity
+ * @Table(name="cms_users", schema="cms")
+ */
+class DDC7079CmsUser
+{
+    /**
+     * @Id @Column(type="integer")
+     * @GeneratedValue
+     */
+    public $id;
+
+    /**
+     * @OneToOne(targetEntity="DDC7079CmsAddress", mappedBy="user", cascade={"persist"}, orphanRemoval=true)
+     */
+    public $address;
+}
+
+/**
+ * @Entity
+ * @Table(name="cms_addresses", schema="cms")
+ */
+class DDC7079CmsAddress
+{
+    /**
+     * @Column(type="integer")
+     * @Id @GeneratedValue
+     */
+    public $id;
+
+    /**
+     * @OneToOne(targetEntity="DDC7079CmsUser", inversedBy="address")
+     * @JoinColumn(referencedColumnName="id")
+     */
+    public $user;
+}
